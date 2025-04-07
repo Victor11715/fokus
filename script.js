e4f86f39d285fdd343de15171b1972bdf077afd4
Victor11715 <victorhugonotari@gmail.com>
2025-04-07 10:51:54 -0300

    Criado a função de alterar o texto do botão, podendo alterar para 'Começar' e 'Parar'.

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const musica = new Audio('/sons/luna-rise-part-one.mp3');
 const audioPlay = new Audio('/sons/play.wav');
 const audioPausa = new Audio('/sons/pause.mp3');
 const audioTempoFinalizado = new Audio('./sons/beep.mp3');
+const iniciarOuPausarBt = document.querySelector('#start-pause span');
 
 let tempoDecorridoEmSegundos = 5;
 let intervaloId = null;
@@ -72,19 +73,32 @@ function alterarContexto(contexto) {
 
 const contagemRegressiva = () => {
     if (tempoDecorridoEmSegundos <= 0) {
-        audioTempoFinalizado.play();
+        // audioTempoFinalizado.play();
+        zerar();
         alert('Tempo finalizado!');
-        clearInterval(intervaloId);
-        tempoDecorridoEmSegundos = 5;
-        startPauseBt.innerHTML = 'Iniciar';
+        return;
+    }
+
+    tempoDecorridoEmSegundos -= 1;
+    console.log(`Temporizador: ${tempoDecorridoEmSegundos}`);
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar);
+
+function iniciarOuPausar() {
+    if (intervaloId) {
+        audioPausa.play();
+        zerar();
         return;
     }
 
     audioPlay.play();
     intervaloId = setInterval(contagemRegressiva, 1000);
+    iniciarOuPausarBt.textContent = "Pausar";
 }
 
 function zerar() {
     clearInterval(intervaloId);
+    iniciarOuPausarBt.textContent = "Começar";
     intervaloId = null;
 }
